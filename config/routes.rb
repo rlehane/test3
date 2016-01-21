227c15464fd7af08d07853c959e656efc3a89f82
Ruth Lehane <rlehane@tcd.ie>
2016-01-21 18:23:33 +0000

    Working app before adding new login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/profile'
 
   get 'pages/landing'
-
+  root 'pages#landing'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
