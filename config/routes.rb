f6162625095e6e2934bf98022d6eb81cc2722752
Ruth Lehane <rlehane@tcd.ie>
2016-01-23 19:13:08 +0000

    updated volunteer menu and links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'volunteers', to: 'pages#volMenu', as: 'volunteers'
   get 'profile', to: 'users#show[user.id] ', as: 'profile'
   resources :users
   
